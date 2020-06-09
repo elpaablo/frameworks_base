Neoteric-OS/android_frameworks_base

Throw exception from addServiceAnnouncement impl
Throw an UnsupportedOperationException from the base implementation of
addServiceAnnouncement and add supporting documentation.

Bug: 155926142
Test: existing CTS
Change-Id: Ib120b650d2a29acdacb9877cdeeba4b782f3f189

telephony/java/android/telephony/mbms/vendor/MbmsDownloadServiceBase.java
@@ -234,7 +234,8 @@ public class MbmsDownloadServiceBase extends IMbmsDownloadService.Stub {
     @Override
     public @MbmsErrors.MbmsError int addServiceAnnouncementFile(
             int subscriptionId, @NonNull byte[] fileContents) {
-        return 0;
+        throw new UnsupportedOperationException("addServiceAnnouncementFile not supported by"
+                + " this middleware.");
     }
 
     /**

elpaablo/frameworks_base

telephony/java/android/telephony/mbms/vendor/MbmsDownloadServiceBase.java
@@ -234,7 +234,8 @@ public class MbmsDownloadServiceBase extends IMbmsDownloadService.Stub {
     @Override
     public @MbmsErrors.MbmsError int addServiceAnnouncement(
             int subscriptionId, @NonNull byte[] contents) {
-        return 0;
+        throw new UnsupportedOperationException("addServiceAnnouncement not supported by"
+                + " this middleware.");
     }
 
     /**