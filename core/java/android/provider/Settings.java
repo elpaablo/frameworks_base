8aead020c90ceeb6aebebf7340fc79de5a228297
minaripenguin <minaripenguin@users.noreply.github.com>
2023-02-11 10:51:51 +0800

    core: Implement system vibration haptics settings
    * insipired from oplus haptics, based from aosp vibration effecs handling
    
    Co-authored-by: Blaster4385 <venkateshchaturvedi12@gmail.com>
    Change-Id: I9d928a902b5ae57073cb5893ac84ab96030ef9e3
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: REV3NT3CH <rev3nt3ch@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6405,6 +6405,33 @@ public final class Settings {
                 "lockscreen_quick_unlock_control";
 
         /**
+         * Back gesture haptics intensity
+         * @hide
+         */
+        public static final String BACK_GESTURE_HAPTIC_INTENSITY = "back_gesture_haptic_intensity";
+
+        /**
+         * Edge scrolling haptics intensity
+         * @hide
+         */
+        @Readable
+        public static final String EDGE_SCROLLING_HAPTICS_INTENSITY = "edge_scrolling_haptics_intensity";
+
+        /**
+         * Brightness sliding haptics intensity
+         * @hide
+         */
+        @Readable
+        public static final String BRIGHTNESS_SLIDER_HAPTICS_INTENSITY = "brightness_slider_haptics_intensity";
+
+        /**
+         * Volume sliding haptics intensity
+         * @hide
+         */
+        @Readable
+        public static final String VOLUME_SLIDER_HAPTICS_INTENSITY = "volume_slider_haptics_intensity";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -12161,12 +12188,6 @@ public final class Settings {
          * @hide
          */
         public static final String BACK_GESTURE_ARROW = "back_gesture_arrow";
-
-        /**
-         * Whether or not to vibrate when back gesture is used
-         * @hide
-         */
-        public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
 
         /**
          * Control whether the process CPU info meter should be shown.
